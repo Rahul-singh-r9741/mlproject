9b9daa11aff1eb3495ef0ca75473ea9fc3c34cb7
Rahul Singh R <rahulsingh11r98@gmail.com>
2025-07-11 13:52:59 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -8,9 +8,15 @@ def error_message_detail(error,error_detail:sys):
     return error_message
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
+    def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message,error_detail)
+        self.error_message = self.get_detailed_error_message(error_message, error_detail)
+
+    def get_detailed_error_message(self, error_message, error_detail):
+        _, _, exc_tb = error_detail.exc_info()
+        filename = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+        return f"Error occurred in python script name [{filename}] line number [{line_number}] error message [{error_message}]"
 
     def __str__(self):
         return self.error_message
